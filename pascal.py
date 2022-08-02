031a90e24e496d43aab91ac5afada7ae8b8f2355
QiuDylan <100895454+QiuDylan@users.noreply.github.com>
2022-08-02 07:14:46 -0700

    Add files via upload

--- pascal.py
+++ pascal.py
@@ -34,9 +34,9 @@ class Solution:
         for n in range(rows):
             list.append([])
             list[n].append(1)
-            for m in range(1, n):
+            for m in range(1, n-1):
                 list[n].append(list[n - 1][m - 1] + list[n - 1][m])
-            if(rows == 1):
+            if(rows != 1):
                 list[n].append(1)
 
         return list
